f441e2af7548e1b71c684c8b0b197132bab95738
thaiphu2k4 <thaiphu2004@gmail.com>
2024-05-25 15:48:33 +0700

    thành phát thay đổi app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './admin/home/home.component';
 import { HeaderComponent } from './admin/header/header.component';
-
+import { TaskComponent } from './admin/task/task.component';
+import { EmployeeComponent } from './admin/employee/employee.component';
+import { ProjectComponent } from './admin/project/project.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: '', component: HeaderComponent }
+  { path: '', component: HeaderComponent },
+  { path: 'task', component: TaskComponent },
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'project', component: ProjectComponent },
+
+
+
   
 ];
 
