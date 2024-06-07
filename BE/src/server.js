c7d5cee90ece687822bccfd9cccc3ab2f381cb05
thaiphu2k4 <thaiphu2004@gmail.com>
2024-06-07 22:15:58 +0700

    asm2 sửa chữa

--- BE/src/server.js
+++ BE/src/server.js
@@ -32,7 +32,7 @@ app.use('/api', projectRoutes);
 app.use('/api', taskRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
